480a6b7b7b032f651926f2cd6b6c91516b2299d2
hassan_aftah <aftahhassan25@gmail.com>
2026-07-22 10:41:56 +0100

    Message

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Message extends Model
 {
-    //
+    protected $fillable = ['conversation_id', 'sender_id', 'sender_type', 'message'];
+
+    public function conversation()
+    {
+        return $this->belongsTo(Conversation::class);
+    }
+
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    public function aiAnalysis()
+    {
+        return $this->hasOne(AiAnalysis::class);
+    }
 }
