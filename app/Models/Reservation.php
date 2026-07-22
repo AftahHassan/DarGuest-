3aa96412b51f20b4e2c9c444c89b4cd6dcfb1441
hassan_aftah <aftahhassan25@gmail.com>
2026-07-22 10:33:21 +0100

    reservation

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -3,8 +3,38 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Reservation extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'guest_id', 'property_id', 'check_in_date', 'check_out_date',
+        'number_of_guests', 'total_price', 'status', 'special_request',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'check_in_date' => 'date',
+            'check_out_date' => 'date',
+            'total_price' => 'decimal:2',
+        ];
+    }
+    public function guest()
+    {
+        return $this->belongsTo(User::class, 'guest_id');
+    }
+
+    public function property()
+    {
+        return $this->belongsTo(Property::class);
+    }
+
+    public function conversation()
+    {
+        return $this->hasOne(Conversation::class);
+    }
 }
