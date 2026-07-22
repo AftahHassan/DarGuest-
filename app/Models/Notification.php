fe19c4807541ae14702fe4a9aeb3a8007d62a168
hassan_aftah <aftahhassan25@gmail.com>
2026-07-22 10:56:24 +0100

    notification

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Notification extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'title', 'content', 'type', 'is_read'];
+
+    protected function casts(): array
+    {
+        return [
+            'is_read' => 'boolean',
+        ];
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function scopeUnread($query)
+    {
+        return $query->where('is_read', false);
+    }
 }
