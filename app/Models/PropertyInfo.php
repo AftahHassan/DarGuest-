b082cfd55b201a3cbc68373f5fab3b89f11fd880
hassan_aftah <aftahhassan25@gmail.com>
2026-07-22 10:29:04 +0100

    property_info_model

--- app/Models/PropertyInfo.php
+++ app/Models/PropertyInfo.php
@@ -3,8 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class PropertyInfo extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'property_id', 'wifi_name', 'wifi_password',
+        'check_in', 'check_out', 'parking', 'parking_info',
+        'access_instructions', 'house_rules',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'parking' => 'boolean',
+        ];
+    }
+
+    public function property()
+    {
+        return $this->belongsTo(Property::class);
+    }
 }
