de370b5caa0b8d5adab28782a415c33a7139ba37
hassan_aftah <aftahhassan25@gmail.com>
2026-07-27 12:12:50 +0100

    web_route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Web\DashboardController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Web\PropertyController;
+use App\Http\Controllers\Web\ConversationController;
+use App\Http\Controllers\Web\ReservationController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -29,6 +32,14 @@ Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard
     Route::put('properties/{property}/info', [PropertyController::class, 'updateInfo'])->name('properties.info.update');
     Route::post('properties/{property}/recommendations', [PropertyController::class, 'storeRecommendation'])->name('properties.recommendations.store');
     Route::delete('recommendations/{recommendation}', [PropertyController::class, 'destroyRecommendation'])->name('properties.recommendations.destroy');
+
+    Route::get('reservations', [ReservationController::class, 'index'])->name('reservations.index');
+    Route::post('reservations', [ReservationController::class, 'store'])->name('reservations.store');
+    Route::get('reservations/{reservation}', [ReservationController::class, 'show'])->name('reservations.show');
+    Route::patch('reservations/{reservation}/status', [ReservationController::class, 'updateStatus'])->name('reservations.status');
+    Route::patch('reservations/{reservation}/cancel', [ReservationController::class, 'cancel'])->name('reservations.cancel');
+
+    Route::post('conversations/{conversation}/messages', [ConversationController::class, 'storeMessage'])->name('conversations.messages.store');
     
 
 });
